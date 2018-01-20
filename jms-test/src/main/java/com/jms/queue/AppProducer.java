4d33d3da2855a2d195c6223bae032bc5286433c4
guoyunlong <823421218@qq.com>
2018-01-20 23:23:48 +0800

    #java jms学习第一次提交

---
+++ jms-test/src/main/java/com/jms/queue/AppProducer.java
@@ -0,0 +1,12 @@
+package com.jms.queue;
+
+/**
+ * Created with IntelliJ IDEA.
+ * @author guoyunlong
+ * Date: 2018/1/20
+ * Time: 23:15
+ * To change this template use File | Settings | File Templates.
+ * Description:
+ */
+public class AppProducer {
+}
